313253739b66c5d092e0748844ea933ebeee2b53
amirhossain67 <58366869+amirhossain67@users.noreply.github.com>
2022-12-03 23:28:58 +0400

    New Update

--- utilities/Libraries/sentence.js
+++ utilities/Libraries/sentence.js
@@ -5,7 +5,7 @@ secret_formulaez = 7
 function knowledge() {
     part_1_1 = ["I"]
     part_1_2 = ["You"]
-    part_1_3 = ["He","She","It","Human","Earth","Alex","Steve","The King","Apple","The Giant","Legend","Hero","Rosy","Doctor","Robber","Killer","Assassin"]
+    part_1_3 = ["He","She","It","Human","Earth","Alex","Steve","The King","Apple","The Giant","Legend","Hero","Rosy","Doctor","Robber","Killer","Assassin","The God"]
     part_1 = [part_1_1,part_1_2,part_1_3]
     
     part_2_1 = ["am","have","had"] // goes for part_1_1
@@ -19,9 +19,9 @@ function knowledge() {
         --->   part_1_3 + part_2_3 + part_3[2]   <---
     */
 
-    part_3_first = ["cool","Assassin","Doctor","Killer","me","Robber","insane","amazing","dead","a beast","developer","scientist","student","an idiot","Legend","Hero","nerd","flying","eating","dying","sitting","watching","looking","searching","boring","bored","from Earth","taller","gamer","artist","stupid"] // goes for am, is, are
-    part_3_second = ["had enough","been killed","been stolen","coins","done your job","life","gone insane","eaten the whole ice cream","written the book","died","been lied to","been destroyed","been dreaming","seen here before","begun the challenge"] // goes for have, have, has
-    part_3_third = ["to go","the opportunity","the chance","to clean the room","enough power","lost","begun","the keys","money","to play all night","to defeat everyone","to become the best","to recover","to start again","to do it"] // goes for had, had, had
+    part_3_first = ["cool","Assassin","Doctor","Killer","me","Robber","insane","amazing","dead","a beast","developer","scientist","student","an idiot","Legend","Hero","nerd","flying","eating","dying","sitting","watching","looking","searching","boring","bored","from Earth","taller","gamer","artist","stupid","Super Star"] // goes for am, is, are
+    part_3_second = ["had enough","been killed","been stolen","coins","done your job","life","gone insane","eaten the whole ice cream","written the book","died","been lied to","been destroyed","been dreaming","seen here before","begun the challenge","to be murdered","to be nice","to eat dirt","to say hi","to answer me"] // goes for have, have, has
+    part_3_third = ["to go","the opportunity","the chance","to clean the room","enough power","lost","begun","the keys","money","to play all night","to defeat everyone","to become the best","to recover","to start again","to do it","to drink orange juice with milk","to break glass","to eat expired food","to be murdured"] // goes for had, had, had
     part_3 = [part_3_first,part_3_second,part_3_third]
 
     parts = [part_1,part_2,part_3]
