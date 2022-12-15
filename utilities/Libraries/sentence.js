288629ce63614925d5715edab71a81374b8df189
amirhossain67 <58366869+amirhossain67@users.noreply.github.com>
2022-12-15 11:11:27 +0400

    New Update

--- utilities/Libraries/sentence.js
+++ utilities/Libraries/sentence.js
@@ -4,8 +4,8 @@ secret_formulaez = 7
 
 function knowledge() {
     part_1_1 = ["I"]
-    part_1_2 = ["You"]
-    part_1_3 = ["He","She","It","Human","Earth","Alex","Steve","The King","Apple","The Giant","Legend","Hero","Rosy","Doctor","Robber","Killer","Assassin","The God"]
+    part_1_2 = ["You","People","Humans","Ghosts","Gods","Animals","Planets"]
+    part_1_3 = ["He","She","It","Human","Earth","Alex","Steve","The King","Apple","The Giant","Legend","Hero","Rosy","Doctor","Robber","Killer","Assassin","The God","Nima","Amin","Amir","Ramin","Mani"]
     part_1 = [part_1_1,part_1_2,part_1_3]
     
     part_2_1 = ["am","have","had"] // goes for part_1_1
@@ -14,14 +14,18 @@ function knowledge() {
     part_2 = [part_2_1,part_2_2,part_2_3]
 
     /* 
-        --->   part_1_1 + part_2_1 + part_3[0]   <---
-        --->   part_1_2 + part_2_2 + part_3[1]   <---
-        --->   part_1_3 + part_2_3 + part_3[2]   <---
+
+            <-------------------------------------------------------->
+            <--->   part_1[0][x] + part_2[0][x] + part_3[x][y]   <--->
+            <--->   part_1[1][x] + part_2[1][x] + part_3[x][y]   <--->
+            <--->   part_1[2][x] + part_2[2][x] + part_3[x][y]   <--->
+            <-------------------------------------------------------->
+
     */
 
-    part_3_first = ["cool","Assassin","Doctor","Killer","me","Robber","insane","amazing","dead","a beast","developer","scientist","student","an idiot","Legend","Hero","nerd","flying","eating","dying","sitting","watching","looking","searching","boring","bored","from Earth","taller","gamer","artist","stupid","SuperStar","kinda sus","weird","cringe","Captain","Bad"] // goes for am, is, are
+    part_3_first =  ["cool","Assassin","Doctor","Killer","me","Robber","insane","amazing","dead","a beast","developer","scientist","student","an idiot","Legend","Hero","nerd","flying","eating","dying","sitting","watching","looking","searching","boring","bored","from Earth","taller","gamer","artist","stupid","SuperStar","kinda sus","weird","cringe","Captain","Bad"] // goes for am, is, are
     part_3_second = ["had enough","been killed","been stolen","coins","done your job","life","gone insane","eaten the whole ice cream","written the book","died","been lied to","been destroyed","been dreaming","seen here before","begun the challenge","to be murdered","to be nice","to eat dirt","to say hi","to answer me","to kill the ender dragon and free the end"] // goes for have, have, has
-    part_3_third = ["to go","the opportunity","the chance","to clean the room","enough power","lost","begun","the keys","money","to play all night","to defeat everyone","to become the best","to recover","to start again","to do it","to drink orange juice with milk","to break glass","to eat expired food","to be murdured","to be bought","to be killed using the Axe"] // goes for had, had, had
+    part_3_third =  ["to go","the opportunity","the chance","to clean the room","enough power","lost","begun","the keys","money","to play all night","to defeat everyone","to become the best","to recover","to start again","to do it","to drink orange juice with milk","to break glass","to eat expired food","to be murdured","to be bought","to be killed using the Axe"] // goes for had, had, had
     part_3 = [part_3_first,part_3_second,part_3_third]
 
     parts = [part_1,part_2,part_3]
@@ -80,14 +84,6 @@ function sumy(key) {
 }
 
 function generate(key) {
-    switch (key.toLowerCase()) {
-        case "dwaa": case "datnip":
-            return "he is a nice person"
-            break
-        case "amirhossainj123":
-            return "An idiot who only knows 2 things in the world (how to suffer and how to code(like a noob))"
-            break
-    }
     generated_string = ""
     knowings = knowledge()
     p1 = knowings[0]; p2 = knowings[1]; p3 = knowings[2]
