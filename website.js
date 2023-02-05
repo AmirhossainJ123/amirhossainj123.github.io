4e630b88bc51a4bf4b0d6bffe1f2cebd40b49fe0
amirhossain67 <58366869+amirhossain67@users.noreply.github.com>
2023-02-05 21:31:39 +0400

    New Update

---
+++ website.js
@@ -0,0 +1,6 @@
+// Start up
+console.log("Started")
+
+
+// Crisp chat
+window.$crisp=[];window.CRISP_WEBSITE_ID="b4db9d72-b002-49a6-9a95-11a315b87392";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
