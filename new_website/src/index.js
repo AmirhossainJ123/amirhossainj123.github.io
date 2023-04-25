f3ccec1d391842f3ee333bb4da40d97a00c52edc
amirhossain67 <58366869+amirhossain67@users.noreply.github.com>
2023-04-26 00:03:33 +0400

    NEW UPDOIT

--- new_website/src/index.js
+++ new_website/src/index.js
@@ -3,32 +3,24 @@ function doci(id) {return document.getElementById(id);}
 function darkmodetoggle() {
     let Nodes = document.body.querySelectorAll("*");
     for (let index = 0; index < Nodes.length; index++) {
-        try {
-        const element = Nodes.item(index);
-        if (element.style.backgroundColor === "white")
-            element.style.backgroundColor = "black"
-        else if (element.style.backgroundColor === "black")
-            element.style.backgroundColor = "white"
-        else if (element.style.backgroundColor.startsWith("rgba("))
-            element.style.backgroundColor = "rgba" + arrtostr(opcolor(element.style.backgroundColor.replaceAll("rgba(","").replaceAll(")","").split(","))) + ")"
-        else if (element.style.backgroundColor.startsWith("rgb("))
-            element.style.backgroundColor = "rgb" + arrtostr(opcolor(element.style.backgroundColor.replaceAll("rgb(","").replaceAll(")","").split(","))) + ")"
-        
-        if (element.style.color === "white")
-            element.style.color = "black"
-        else if (element.style.color === "black")
-            element.style.color = "white"
-        else if (element.style.color.startsWith("rgba("))
-            element.style.color = "rgba" + arrtostr(opcolor(element.style.color.replaceAll("rgba(","").replaceAll(")","").split(","))) + ")"
-        else if (element.style.color.startsWith("rgb("))
-            element.style.color = "rgb" + arrtostr(opcolor(element.style.color.replaceAll("rgb(","").replaceAll(")","").split(","))) + ")"
-        } catch(err) {
-            console.log(err);
+        if (toggle) {
+            Nodes.item(index).classList.remove("darkmode")
+            document.body.classList.remove("bodark");
+        }
+        else if (!Nodes.item(index).classList.contains("NoDarkMode")) {
+            Nodes.item(index).classList.add("darkmode")
+            document.body.classList.add("bodark");
+        }
+        if (Nodes.item(index).classList.contains("LightShadeDarkMode")) {
+            if (toggle) {
+                Nodes.item(index).classList.remove("lsdm")
+            }
+            else {
+                Nodes.item(index).classList.add("lsdm")
+            }
         }
     }
 }
-function opcolor(array) {array[0] = parseInt(255-array[0]) + ","; array[1] = parseInt(255-array[1]) + ","; array[2] = parseInt(255-array[2]); return array}
-function arrtostr(arr) {let finalstring = ""; arr.forEach((e) => {finalstring+=e;}); return arr;}
 function clickedonimage() {
     if (toggle)
         doci("logo").className = "bluemode redmode"
